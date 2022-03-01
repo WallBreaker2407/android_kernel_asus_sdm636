36ccd4557dd14646eb4f610baf87dd9d0307b3ad
WallBreaker2407 <wallbreaker2407@gmail.com>
2022-03-01 10:52:08 +0000

    cpufreq: add and enable Lionheart governor Change-Id: Icb84d9c96c345d816800e70c80168708c0ebc026 Signed-off-by: ben863 <r.budhiono@gmail.com> Signed-off-by: WallBreaker2407 <wallbreaker2407@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -521,6 +521,9 @@ extern struct cpufreq_governor cpufreq_gov_userspace;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
